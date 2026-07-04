faee34453c9947739256220c72ac30109d0919ac
omipaz1234-hue <omipaz.1234@gmail.com>
2026-07-04 00:34:22 -1200

    Update inicio.py

--- modules/inicio.py
+++ modules/inicio.py
@@ -7,11 +7,9 @@ def mostrar():
     col1, col2, col3 = st.columns([1.2, 3, 1.4])
 
     with col1:
-
         st.image("assets/panda.png", width=170)
 
     with col2:
-
         st.markdown("""
         # PANDA STATISTIC LIFE
 
@@ -20,26 +18,26 @@ def mostrar():
         """)
 
     with col3:
-
         st.image("assets/bolivia.png", width=180)
 
     st.divider()
-c1, c2, c3, c4, c5, c6 = st.columns(6)
 
-with c1:
-    st.button("📊 Estadística", use_container_width=True)
+    c1, c2, c3, c4, c5, c6 = st.columns(6)
 
-with c2:
-    st.button("🦠 Bioestadística", use_container_width=True)
+    with c1:
+        st.button("📊 Estadística", use_container_width=True)
 
-with c3:
-    st.button("👥 Demografía", use_container_width=True)
+    with c2:
+        st.button("🦠 Bioestadística", use_container_width=True)
 
-with c4:
-    st.button("🛡 Vigilancia", use_container_width=True)
+    with c3:
+        st.button("👥 Demografía", use_container_width=True)
 
-with c5:
-    st.button("🗄 SNIS", use_container_width=True)
+    with c4:
+        st.button("🛡 Vigilancia", use_container_width=True)
 
-with c6:
-    st.button("📈 Canal Endémico", use_container_width=True)
+    with c5:
+        st.button("🗄 SNIS", use_container_width=True)
+
+    with c6:
+        st.button("📈 Canal Endémico", use_container_width=True)
